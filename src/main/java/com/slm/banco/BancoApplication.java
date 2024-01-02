7f6c9d5b2545564d37aef04719565e9cab65ad84
Salomao Machado <salomao.machado.75@gmail.com>
2024-01-01 21:46:20 -0300

    first commit

---
+++ src/main/java/com/slm/banco/BancoApplication.java
@@ -0,0 +1,29 @@
+package com.slm.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+	}
+
+}
+
+@RestController
+class HttpController{
+	@GetMapping("/public")
+	String publicRoute(){
+		return "<h1>Rota publica</h1>";
+	}
+	@GetMapping("/private")
+	String privateRoute(){
+		return "<h1>Rota privada</h1>";
+	}
+}
