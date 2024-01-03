d0287cfc061e8b2e74ff60c2428e4a259fdfdcee
Salomao Machado <salomao.machado.75@gmail.com>
2024-01-03 00:17:53 -0300

    Feat: Adicionando parametros e iniciando classes para as contas

--- src/main/java/com/slm/banco/account/Account.java
+++ src/main/java/com/slm/banco/account/Account.java
@@ -1,11 +1,21 @@
 package com.slm.banco.account;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Table(name = "account")
 @Entity(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String accountNumber; // Número da conta
+//    private String accountType;   // Tipo de conta (corrente, poupança, etc.)
+//    private BigDecimal balance;   // Saldo da conta
+//    private Date dateOpened;       // Data de abertura da conta
+//    private boolean isActive;      // Estado da conta (ativa/inativa)
 }
--- src/main/java/com/slm/banco/controller/AccountController.java
+++ src/main/java/com/slm/banco/controller/AccountController.java
@@ -1,16 +1,25 @@
 package com.slm.banco.controller;
 
 import com.slm.banco.account.Account;
+import com.slm.banco.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("account")
 public class AccountController {
 
+    @Autowired
+    private AccountRepository repository;
     @GetMapping
-    public void getAll(){
-        Account accout
+    public List<Account> getAll(){
+
+        List<Account> accountList = repository.findAll();
+        return accountList;
+
     }
 }
